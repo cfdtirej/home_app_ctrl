e22c15dc2bcb4a44eac9b4f32b31a439e51d525f
cfdtirej <53041956+cfdtirej@users.noreply.github.com>
2021-03-21 21:36:12 +0900

    fix

--- src/ctrl_appliances_rec.py
+++ src/ctrl_appliances_rec.py
@@ -1,61 +1,22 @@
-import csv
 import os
-import datetime
+import csv
 import json
-import time
-from dataclasses import dataclass, asdict
-from typing import List, Optional, Any, Dict, NoReturn, Literal
+from dataclasses import dataclass
+from datetime import datetime
 from pathlib import Path
+from typing import List, Dict, Optional, Any
 
 import pytz
 from remo import NatureRemoAPI
-from influxdb import InfluxDBClient
 
 import calc_di_tm
 import settings
 
 
-api = NatureRemoAPI(settings.API_HAKUSAN)
 jst = pytz.timezone('Asia/Tokyo')
 
-# def appliance_csv_writer():
 
-
-class NatureRemoGet(NatureRemoAPI)
-def get_applience_state() -> List[Dict[str, Any]]:
-    result = []
-    for appliance in api.get_appliances():
-        if appliance.aircon:
-            aircon_setting = json.loads(appliance.settings.as_json_string())
-            aircon = {
-                'updated_at':  appliance.device.updated_at.astimezone(jst).isoformat(),
-                'name': appliance.device.name,
-                'mac_address': appliance.device.mac_address,
-                **aircon_setting
-            }
-            result.append(aircon)
-
-        elif appliance.light:
-            light_state = json.loads(appliance.light.state.as_json_string()) 
-            light = {
-                'update_at': appliance.device.updated_at.astimezone(jst).isoformat(),
-                'name': appliance.device.name,
-                'mac_address': appliance.device.mac_address,
-                **light_state
-            }
-            result.append(light)
-        elif appliance.tv:
-            tv_state = json.loads(appliance.tv.state.as_json_string())
-            tv = {
-                'update_at': appliance.device.updated_at.astimezone(jst).isoformat(),
-                'name': appliance.device.name,
-                'mac_address': appliance.device.mac_address,
-                **tv_state
-            }
-            result.append(tv)
-    return result
-
-class GetHakusanNatureRemo(NatureRemoAPI):
+class HakusanAppliancesCtrl(NatureRemoAPI):
     def get_appliances_state(self) -> List[Dict[str, Any]]:
         result = []
         for appliance in self.get_appliances():
@@ -94,22 +55,58 @@ class GetHakusanNatureRemo(NatureRemoAPI):
                 result.append(tv)
         return result
     
-    def appliance_settings_csv_writer(self, state: Dict[str, Any]) -> NoReturn:
-        dt = datetime.datetime.now(jst)
-        app_type = state['type']
-        csvfile = Path(__file__).parent / 'logcsv' / 'home_app' / f'{app_type}' / f'{dt.date()}.csv'
-        if not csvfile.parent.is_dir():
-            csvfile.parent.mkdir(parents=True)
+    def get_aircon_settings(self, appliance: str) -> Dict[str, Any]:
+        """
+        Args:
+            appliance: aircon id
+        Returns:
+            Aircon settings
+        """
+        for app in self.get_appliances():
+            if app.id == appliance:
+                aircon_settings = {
+                    'update_at': app.device.updated_at.astimezone(jst),
+                    'id': app.id,
+                    'nickname': app.nickname,
+                    'type': app.type,
+                    'button': app.settings.button,
+                    'dir': app.settings.dir,
+                    'mode': app.settings.mode,
+                    'vol': app.settings.vol,
+                    'temp': app.settings.temp
+                }
+                return aircon_settings
+
+    def update_aircon_settings_rec(
+        self, appliance: str, operation_mode: str=None, temperature: str=None, air_volume: str=None, air_direction: str=None, button: str=None
+    ) -> None:
+        """param appliance: str
+        Update air conditioner settings.
+        Args:
+            appliance: Appliance ID.
+            operation_mode: AC operation mode.
+            temperature: Temperature.
+            air_volume: AC air volume.
+            air_direction: AC air direction.
+            button: Button.
+        """
+        self.update_aircon_settings()
+        self.update_aircon_settings(appliance, operation_mode, temperature, air_volume, air_direction, button)
+        aircon_settings = self.get_aircon_settings(appliance)
+        dt = datetime.now(tz=jst).date()
+        nickname = aircon_settings['nickname']
+        csvfile = Path(__file__).parent / 'appliances' / f'{nickname}' / f'{dt}.csv'
+        if not csvfile.is_dir():
+            csvfile.mkdir(parents=True)
         if not csvfile.exists():
             csvfile.touch()
             with open(csvfile, 'w') as f:
                 writer = csv.writer(f)
-                writer.writerow(state.keys())
+                writer.writerow(aircon_settings.keys)
         with open(csvfile, 'a') as f:
             writer = csv.writer(f)
-            writer.writerow(state.values())
-            
-    
+            writer.writerow(aircon_settings.values())
+
     def get_hu_il_te(self) -> List[Dict[str, Any]]:
         """Nature Remo API から室内環境情報を取得
         Returns:
@@ -135,9 +132,8 @@ class GetHakusanNatureRemo(NatureRemoAPI):
         result = []
         for device in self.get_devices():
             result.append({
-                'time': datetime.datetime.now(tz=pytz.timezone('Asia/Tokyo')).isoformat(),
+                'time': datetime.datetime.now(tz=jst).isoformat(),
                 'name': device.name,
-                'mac_address': device.mac_address,
                 'te': device.newest_events['te'].val,
                 'il': device.newest_events['il'].val,
                 'hu': device.newest_events['hu'].val,
@@ -146,8 +142,8 @@ class GetHakusanNatureRemo(NatureRemoAPI):
                 'di_lebel': calc_di_tm.di_lebel(calc_di_tm.calc_di(device.newest_events['te'].val, device.newest_events['hu'].val))
             })
         return result
-
-    def room_state_csv_writer(self, room_state: List[Dict[str, Any]]) -> NoReturn:
+    
+    def room_state_csv_writer(self, room_state: List[Dict[str, Any]]) -> None:
         """室内環境データのCSV記録
         Args:
             room_state: self.get_hu_il_te() Returns
@@ -155,7 +151,7 @@ class GetHakusanNatureRemo(NatureRemoAPI):
         dt = datetime.datetime.now(jst)
         for state in room_state:
             # 室内環境データ用のcsvファイルの確認と作成
-            csvfile = Path(__file__).parent / 'logcsv' / 'room' / f'{dt.date()}.csv'
+            csvfile = Path(__file__).parent / 'room_state' / f'{state["name"]}' / f'{dt.date()}.csv'
             if not csvfile.parent.is_dir():
                 csvfile.parent.mkdir(parents=True)
             if not csvfile.exists():
@@ -166,12 +162,3 @@ class GetHakusanNatureRemo(NatureRemoAPI):
             with open(csvfile, 'a') as f:
                 writer = csv.writer(f)
                 writer.writerow(state.values())
-
-
-if __name__ == '__main__':
-    api = GetHakusanNatureRemo(settings.API_HAKUSAN)
-    ap = api.get_appliances_state()
-    print(ap)
-    # api.appliance_settings_csv_writer(ap)
-    # print(api.get_hu_il_te())
-    # api.record_room_state()
